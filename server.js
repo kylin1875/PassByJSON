94ffb2e58d0f5ac3bd07e4a88d6992f1c4e2be6d
kylin1875 <zhiweizheng@Zhiweis-MacBook-Pro.local>
2016-07-28 15:55:46 -0700

    test

---
+++ server.js
@@ -0,0 +1,63 @@
+var http = require('http')
+var url = require('url')
+var class = [ 
+	{
+		 "Course": "CS551",
+		 "Homework":"1",
+		 "Score":"84"
+	},
+	{
+		 "Course": "CS551",
+		 "Homework":"2",
+		 "Score":"93"
+	},
+	{
+		 "Course": "CS551",
+		 "Homework":"3",
+		 "Score":"88"
+	},
+	{
+		 "Course": "CS557",
+		 "Homework":"1",
+		 "Score":"90"
+	},
+	{
+		 "Course": "CS557",
+		 "Homework":"2",
+		 "Score":"85"
+	}
+]
+
+function returnValue(class) {
+	if(class=="cs551"){
+		return{
+				score : class[0].score,
+				score : class[1].score,
+				score : class[2].score
+		}
+	}else{
+		return{ 
+			score : class[3].score,
+			score : class[4].score
+		}
+	}
+}
+
+var server = http.createServer(function (req, res) {
+	var parsedUrl = url.parse(req.url, true)
+	var class = new Date(parsedUrl.query.course_id)
+	var result = returnValue(time)
+	if (result) {
+		res.writeHead(200, { 'Content-Type': 'application/json' })
+		res.end(JSON.stringify(result))
+	} else {
+		res.writeHead(404)
+		res.end()
+	}
+})
+
+var port = process.env.PORT || 3000;
+server.listen(port, function(){ 
+	console.log('listening on *'+port);
+});p
+		
